d5808254e215b8fa496e4f5ae5cc1b60fe33dc64
wraps <me@matthieu.pw>
2023-11-24 00:42:01 +0100

    KHV-3-cart + component

--- Styles/Styles.tsx
+++ Styles/Styles.tsx
@@ -25,6 +25,57 @@ const commonStyles = StyleSheet.create({
     top: -250,
     zIndex: 1,
   },
+  // start cart
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  },
+  modalButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%'
+  },
+  cartItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    borderRadius: 10,
+    borderBottomColor: '#ddd',
+  },
+  cartImage: {
+    width: 100,
+    height: 100,
+    marginRight: 15,
+  },
+  quantityControls: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  quantityText: {
+    marginHorizontal: 10,
+  },
+  cartContainer: {
+    flex: 1,
+    padding: 25,
+    paddingTop: 50,
+  },
+  cartHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 25,
+  },
+  cartTotal: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  // end cart
 });
 
 export default commonStyles;
