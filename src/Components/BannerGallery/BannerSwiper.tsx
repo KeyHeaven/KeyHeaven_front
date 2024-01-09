9c2058cf8204e140a6876d851f4a02e7513ebc41
wassimouricha <wbouricha5@gmail.com>
2024-01-09 09:19:59 +0100

    commentaire

--- src/Components/BannerGallery/BannerSwiper.tsx
+++ src/Components/BannerGallery/BannerSwiper.tsx
@@ -16,6 +16,7 @@ const BannerSwiper: React.FC<BannerSwiperProps> = ({ data, onPress }) => {
   const swiperRef = useRef<Swiper>(null);
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  // fonction permettant de faire défiler les slides automatiquement
   useEffect(() => {
     const autoSwipe = setInterval(() => {
       if (swiperRef.current) {
