9f3e06ec3599ecd9eb67c59d934555c6832147d2
wraps <me@matthieu.pw>
2024-02-09 13:45:50 +0100

    Fix axios base url

--- axiosConfig.tsx
+++ axiosConfig.tsx
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL;
 
 const instance = axios.create({
-    baseURL: "http://192.168.1.25:8000/api",
+    baseURL: API_BASE_URL,
 });
 
 instance.interceptors.request.use(async (config) => {
