dfb936bde9c204ea18810e2fadf7fc23e3353059
wraps <me@matthieu.pw>
2024-02-09 13:38:46 +0100

    feat(TabContent): dynamic slide fetching based on active tab

--- src/Components/TabContent/TabContent.tsx
+++ src/Components/TabContent/TabContent.tsx
@@ -1,19 +1,46 @@
-import React from "react";
+import React, {useEffect, useState } from "react";
 import { ScrollView } from "react-native";
 import commonStyles from "../../../Styles/Styles";
 import NewTabGallery from "../TabGallery/NewTabGallery";
 import BestSellsGallery from "../TabGallery/BestSellsGallery";
-import slides from "../../../Data/Slides";
 import SalesGallery from "../TabGallery/SalesGallery";
-import BestSellsSlides from "../../../Data/BestSellsSlides";
-import OfferSlides from "../../../Data/OfferSlides";
 import handleCardPress from "../../../logic/handleCardPress";
-
+import {getGames} from "../../Controllers/GameController";
 interface TabContentProps {
   activeTab: string;
 }
 
 const TabContent: React.FC<TabContentProps> = ({ activeTab }) => {
+  const [slides, setSlides] = useState([]);
+  const [bestSellsSlides, setBestSellsSlides] = useState([]);
+  const [offerSlides, setOfferSlides] = useState([]);
+
+  useEffect(() => {
+    const fetchSlides =async  () => {
+      const data = await getGames();
+      setSlides(data['hydra:member']);
+    }
+
+    const fetchBestSellsSlides =async () => {
+      const data = await getGames();
+      // sort data aleatory
+        data['hydra:member'].sort(() => Math.random() - 0.5);
+      setBestSellsSlides(data['hydra:member']);
+    }
+    const fetchOfferSlides = async() => {
+      const data = await getGames();
+      data['hydra:member'].sort(() => Math.random() - 0.5);
+      setOfferSlides(data['hydra:member']);
+    }
+
+    if (activeTab === "Tab1") {
+      fetchSlides();
+    } else if (activeTab === "Tab2") {
+      fetchBestSellsSlides();
+    } else if (activeTab === "Tab3") {
+      fetchOfferSlides();
+    }
+  }, [activeTab]);
 
   switch (activeTab) {
     case "Tab1":
@@ -25,13 +52,13 @@ const TabContent: React.FC<TabContentProps> = ({ activeTab }) => {
     case "Tab2":
       return (
         <ScrollView style={commonStyles.tabContent}>
-          <BestSellsGallery data={BestSellsSlides} onPress={handleCardPress} />
+          <BestSellsGallery data={bestSellsSlides} onPress={handleCardPress} />
         </ScrollView>
       );
     case "Tab3":
       return (
         <ScrollView style={commonStyles.tabContent}>
-          <SalesGallery data={OfferSlides} onPress={handleCardPress} />
+          <SalesGallery data={offerSlides} onPress={handleCardPress} />
         </ScrollView>
       );
     default:
