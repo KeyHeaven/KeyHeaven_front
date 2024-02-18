e4f4c097a8a68396df6d8a086e9d46b160533263
wraps <me@matthieu.pw>
2024-02-18 23:55:08 +0100

    Add reviewController and modal

---
+++ src/Components/GameScreenComponent/GameReviewsSection.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import gameStyles from '../../../Styles/GameStyles';
+
+const GameReviewsSection = ({ reviews }) => {
+    if (!reviews || reviews.length === 0) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>Aucun commentaire disponible</Text>
+            </View>
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={gameStyles.textTitle}>Commentaires des utilisateurs</Text>
+            {reviews.map((review, index) => (
+                <View key={index} style={styles.review}>
+                    <Text style={gameStyles.text}>{review.username}</Text>
+                    <Text style={gameStyles.text}>{review.comment}</Text>
+                    <Text style={gameStyles.text}>Note : {review.rating} / 5</Text>
+                </View>
+            ))}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 10,
+        paddingRight: 10,
+        display: "flex",
+        width: "95%",
+        backgroundColor: "#323232",
+        borderRadius: 10,
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    review: {
+        marginBottom: 10,
+    },
+    username: {
+        fontWeight: 'bold',
+    },
+});
+
+export default GameReviewsSection;
---
+++ src/Controllers/ReviewController.tsx
@@ -0,0 +1,32 @@
+import axiosInstance from '../../axiosConfig';
+
+export const getReviewsByGameId = async (id: string) => {
+    try {
+        const response = await axiosInstance.get(`/reviews?game=${id}`);
+        return response.data;
+    } catch (error: any) {
+        throw new Error('Erreur lors de la récupération des avis :', error.code);
+    }
+}
+
+export const addReview = async (gameId: number, userId: number, rating: number, comment: string) => {
+    try {
+        const data = {
+            "gameId": '/api/games/' + gameId,
+            "userId": '/api/users/' + userId,
+            "note": rating,
+            "comment": comment,
+            "submitAt": new Date().toISOString()
+        };
+        console.log('Data:', data);
+        const response = await axiosInstance.post('/reviews', data, {
+            headers: {
+                'Content-Type': 'application/ld+json'
+            }
+
+        });
+        return response.data;
+    } catch (error: any) {
+        throw new Error('Erreur lors de l\'ajout de l\'avis :', error.title);
+    }
+}
