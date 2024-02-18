8eba7954dfe46ac5c145a947fe9b6696505d14b6
wraps <me@matthieu.pw>
2024-02-19 00:15:20 +0100

    Add guard and protect routes

--- Navigations/Navigation.tsx
+++ Navigations/Navigation.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { StatusBar } from "expo-status-bar";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StatusBar, View, ActivityIndicator } from "react-native";
 import HomeScreen from "../Screens/HomeScreen";
 import DetailsScreen from "../Screens/DetailsScreen";
 import SplashScreen from "../Screens/SplashAnimation/SplashScreen";
@@ -22,32 +23,64 @@ import SupportScreen from "../Screens/Support/SupportScreen";
 
 const Stack = createStackNavigator();
 
-const Navigation: React.FC = () => (
-  <NavigationContainer>
-    <Stack.Navigator
-      initialRouteName="Start"
-      screenOptions={{ headerShown: false }}>
-      {/* Chaque écrans doit etre indiqué ici  */}
-      <Stack.Screen name="Start" component={StartScreen} />
-      <Stack.Screen name="Splash" component={SplashScreen} />
-      <Stack.Screen name="Auth" component={AuthScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="SignUp" component={SignUpScreen} />
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Details" component={DetailsScreen} />
-      <Stack.Screen name="Game" component={GameScreen} />
-      <Stack.Screen name="Cart" component={CartScreen} />
-        <Stack.Screen name="Payment" component={PaymentScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="PersonalInformation" component={PersonalInformationScreen} />
-        <Stack.Screen name="OdersHistory" component={OrderHistoryScreen} />
-        <Stack.Screen name="ChangePassword" component={ChangePasswordScreen} />
-        <Stack.Screen name="PurchaseDetailsScreen" component={PurchaseDetailsScreen} />
-        <Stack.Screen name="Search" component={SearchScreen} />
-      <Stack.Screen name="SupportScreen" component={SupportScreen} />
-    </Stack.Navigator>
-    <StatusBar style="auto" />
-  </NavigationContainer>
-);
+const Navigation: React.FC = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const bootstrapAsync = async () => {
+      let userToken;
+
+      try {
+        userToken = await AsyncStorage.getItem('userToken');
+      } catch (e) {
+        console.error("Restoring token failed", e);
+      }
+
+      setIsAuthenticated(!!userToken);
+    };
+
+    bootstrapAsync();
+  }, []);
+
+  if (isAuthenticated === null) {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <ActivityIndicator />
+        </View>
+    );
+  }
+
+  return (
+      <NavigationContainer>
+        <Stack.Navigator
+            screenOptions={{ headerShown: false }}>
+          {isAuthenticated ? (
+              <>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Details" component={DetailsScreen} />
+                <Stack.Screen name="Game" component={GameScreen} />
+                <Stack.Screen name="Cart" component={CartScreen} />
+                <Stack.Screen name="Payment" component={PaymentScreen} />
+                <Stack.Screen name="Profile" component={ProfileScreen} />
+                <Stack.Screen name="PersonalInformation" component={PersonalInformationScreen} />
+                <Stack.Screen name="OdersHistory" component={OrderHistoryScreen} />
+                <Stack.Screen name="ChangePassword" component={ChangePasswordScreen} />
+                <Stack.Screen name="PurchaseDetailsScreen" component={PurchaseDetailsScreen} />
+                <Stack.Screen name="Search" component={SearchScreen} />
+                <Stack.Screen name="SupportScreen" component={SupportScreen} />
+              </>
+          ) : (
+              <>
+                <Stack.Screen name="Start" component={StartScreen} />
+                <Stack.Screen name="Splash" component={SplashScreen} />
+                <Stack.Screen name="Auth" component={AuthScreen} />
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="SignUp" component={SignUpScreen} />
+              </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+  );
+};
 
 export default Navigation;
