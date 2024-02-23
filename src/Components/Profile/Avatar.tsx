9a1627892396c9ab5bcbd2ff6169c34d74109015
wraps <me@matthieu.pw>
2024-02-23 01:48:42 +0100

    Update Personnal information

--- src/Components/Profile/Avatar.tsx
+++ src/Components/Profile/Avatar.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const Avatar = ({ uri }) => (
     <View style={styles.avatarContainer}>
-        <Image source={{ uri }} style={styles.avatar} />
+        <View style={styles.avatarOverlay}>
+            <Image source={{ uri }} style={styles.avatar} />
+            <TouchableOpacity style={styles.editIcon} >
+                <FontAwesome name="pencil" size={20} color="#fff" />
+            </TouchableOpacity>
+        </View>
     </View>
 );
 
@@ -12,12 +18,22 @@ const styles = StyleSheet.create({
         marginBottom: 30,
         alignItems: 'center',
     },
+    avatarOverlay: {
+        position: 'relative',
+    },
     avatar: {
         width: 100,
         height: 100,
-        borderRadius: 10,
+        borderRadius: 50,
         backgroundColor: '#555',
-        marginBottom: 20,
+    },
+    editIcon: {
+        position: 'absolute',
+        right: -10,
+        bottom: 0,
+        backgroundColor: 'royalblue',
+        borderRadius: 20,
+        padding: 6,
     },
 });
 
