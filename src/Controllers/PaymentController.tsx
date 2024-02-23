b5ebf697c14eda5327e3e0f5d18d5551963297bf
wraps <me@matthieu.pw>
2024-02-23 20:10:33 +0100

    Add createStripeCustomer

--- src/Controllers/PaymentController.tsx
+++ src/Controllers/PaymentController.tsx
@@ -1,11 +1,35 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
+import {jwtDecode} from 'jwt-decode';
+import axiosInstance from '../../axiosConfig';
+import { getUserById } from "./userController";
 
 export const createPaymentIntent = async (items: any) => {
+    let stripeId = "";
+    const token = await AsyncStorage.getItem('userToken');
+    if (token) {
+        const decodedToken: any = jwtDecode(token);
+        const user = await getUserById(decodedToken.id);
+        stripeId = user.stripeId;
+
+        if (!stripeId) {
+            try {
+                const customerResponse = await createStripeCustomer(user.email);
+                console.log('CustomerResponse:', customerResponse);
+                stripeId = customerResponse.stripeId;
+            } catch (error: any) {
+                throw new Error('Erreur lors de la création du client Stripe :', error.code);
+            }
+        }
+    }
+
+    const data = {
+        customerId: stripeId,
+        items: items,
+    };
+    console.log('Data:', data);
     try {
-        const response = await axios.post(`${process.env.EXPO_PUBLIC_API_PAYMENT_URL}/create-payment-intent`, {
-            customerId: 'cus_PAran2pH3B5egS',
-            items: items,
-        }, {
+        const response = await axiosInstance.post(`/create-payment-intent`, data, {
             headers: {
                 'Content-Type': 'application/json',
             }
@@ -16,8 +40,9 @@ export const createPaymentIntent = async (items: any) => {
     }
 }
 export const paymentSuccess = async (paimentIntentId: string, purchaseId: number) => {
+    console.log('PaymentSuccess:', paimentIntentId, purchaseId);
     try {
-        const response = await axios.post(`${process.env.EXPO_PUBLIC_API_PAYMENT_URL}/payment-success`, {
+        const response = await axiosInstance.post(`/payment-success`, {
             paymentIntentId: paimentIntentId,
             purchaseId: purchaseId
         }, {
@@ -28,5 +53,20 @@ export const paymentSuccess = async (paimentIntentId: string, purchaseId: number
         return response.data;
     } catch (error: any) {
         throw new Error('Erreur lors de la mise à jour de la commande :', error.code);
+    }
+}
+
+export const createStripeCustomer = async (email: string) => {
+    try {
+        const response = await axiosInstance.post(`/create-stripe-customer`, {
+            email: email
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        return response.data;
+    } catch (error: any) {
+        throw new Error('Erreur lors de la création du client Stripe :', error.code);
     }
 }
