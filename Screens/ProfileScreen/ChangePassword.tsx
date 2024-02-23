d3b5408f2b692ead63b5fe13be3ff99dcc383b48
wraps <me@matthieu.pw>
2024-02-23 01:47:22 +0100

    Add updatePassword

--- Screens/ProfileScreen/ChangePassword.tsx
+++ Screens/ProfileScreen/ChangePassword.tsx
@@ -1,21 +1,48 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 import commonStyles from '../../Styles/Styles';
 import CustomButton from '../../src/Components/button/CustomBtnComponent';
 import CustomInput from '../../src/Components/input/CustomInput';
 import { faLock } from '@fortawesome/free-solid-svg-icons';
 import { ChangePasswordScreenProps } from '../../Navigations/NavigationType';
-import GoBack from '../../src/Components/Profile/GoBack';
+import TopBar from '../../src/Components/TopBar/TopBar';
+import { updatePassword } from '../../src/Controllers/AuthentificationController';
 
 const ChangePasswordScreen: React.FC<ChangePasswordScreenProps> = ({navigation}) => {
     const [currentPassword, setCurrentPassword] = useState('');
     const [newPassword, setNewPassword] = useState('');
     const [confirmNewPassword, setConfirmNewPassword] = useState('');
 
+    const handleChangePassword = async () => {
+        if (!currentPassword || !newPassword || !confirmNewPassword) {
+            Alert.alert('Erreur', 'Veuillez remplir tous les champs.');
+            return;
+        }
+
+        if (newPassword !== confirmNewPassword) {
+            Alert.alert('Erreur', 'Le nouveau mot de passe et la confirmation ne correspondent pas.');
+            return;
+        }
+
+        try {
+            const response = await updatePassword('user2@example.com',currentPassword,newPassword);
+
+            if (response.ok) {
+                Alert.alert('Succès', 'Le mot de passe a été changé avec succès.');
+                navigation.navigate("Home");
+            } else {
+                Alert.alert('Erreur', 'Une erreur est survenue lors du changement de mot de passe.');
+            }
+        } catch (error) {
+            console.error("Erreur lors du changement de mot de passe:", error);
+            Alert.alert('Erreur', 'Une erreur est survenue lors du changement de mot de passe.');
+        }
+    };
+
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
             <View style={commonStyles.containerHomePage}>
-                <GoBack />
+                <TopBar showBackButton={true} />
                 <View style={{paddingTop: 50, flex: 1}} >
                     <Text style={styles.header}>Modifier mon mot de passe</Text>
 
@@ -25,7 +52,7 @@ const ChangePasswordScreen: React.FC<ChangePasswordScreenProps> = ({navigation})
                 </View>
                 <View style={{ alignItems: "center" }}>
                     <CustomButton
-                        onPress={() => navigation.navigate("Home")}
+                        onPress={handleChangePassword}
                         buttonText="Sauvegarder"
                     />
                 </View>
--- src/Controllers/AuthentificationController.tsx
+++ src/Controllers/AuthentificationController.tsx
@@ -34,3 +34,24 @@ export const register = async (email: string, password: string) => {
         throw error;
     }
 }
+
+export const updatePassword = async (email: string, oldPassword: string, newPassword: string) => {
+    console.log(email, oldPassword, newPassword);
+    const data = {
+        email: email,
+        old_password: oldPassword,
+        new_password: newPassword
+    }
+    console.log(data);
+    try {
+        const response = await axiosInstance.post(`/change-password`, data, {
+            headers: {
+                'Content-Type': 'application/ld+json'
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Erreur lors du changement de mot de passe:", error);
+        throw error;
+    }
+}
